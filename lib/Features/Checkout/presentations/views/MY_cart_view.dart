21e0f3bf395b3af74a3475a358fb1547743f9643
ALi1910 <alihany5559@gmail.com>
2023-10-26 17:15:17 +0300

    AppBar Done

---
+++ lib/Features/Checkout/presentations/views/MY_cart_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:payment/core/utils/styles.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Center(
+          child: SvgPicture.asset('assets/images/Arrow.svg'),
+        ),
+        title: const Text(
+          'My Cart',
+          style: Styles.style25,
+        ),
+      ),
+    );
+  }
+}
